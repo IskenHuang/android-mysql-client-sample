a0d9a1950d635e45b5043fc4483b5554d746a1cb
Isken Huang <kensheep@gmail.com>
2012-09-16 23:36:27 +0800

    add list view

--- src/com/iskenhuang/gear/GMainActivity.java
+++ src/com/iskenhuang/gear/GMainActivity.java
@@ -1,10 +1,14 @@
 package com.iskenhuang.gear;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class GMainActivity extends Activity {
 
@@ -12,6 +16,7 @@ public class GMainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gmain_activity);
+        init();
     }
 
     @Override
@@ -20,5 +25,20 @@ public class GMainActivity extends Activity {
         return true;
     }
 
-    
+    private void init(){
+    	ListView listView = (ListView) findViewById(R.id.mylist);
+		String[] values = new String[] { "connect MySQL" };
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, android.R.id.text1, values);
+		listView.setAdapter(adapter);
+		listView.setOnItemClickListener(new OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				Toast.makeText(getApplicationContext(),
+						"Click ListItem Number " + position, Toast.LENGTH_LONG)
+						.show();
+			}
+		});
+    }
 }
