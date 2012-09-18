7481e30736c4be2af0addb356eb9e109d695557a
Isken Huang <kensheep@gmail.com>
2012-09-18 22:45:10 +0800

    rename project

--- gen/com/iskenhuang/gear/R.java
+++ gen/com/iskenhuang/gear/R.java
@@ -21,13 +21,11 @@ public final class R {
     }
     public static final class id {
         public static final int buttonResult=0x7f080000;
-        public static final int menu_settings=0x7f080003;
-        public static final int mylist=0x7f080002;
+        public static final int menu_settings=0x7f080002;
         public static final int textResult=0x7f080001;
     }
     public static final class layout {
-        public static final int display_result=0x7f030000;
-        public static final int gmain_activity=0x7f030001;
+        public static final int gmain_activity=0x7f030000;
     }
     public static final class menu {
         public static final int gmain_activity=0x7f070000;
--- src/com/iskenhuang/gear/GMainActivity.java
+++ src/com/iskenhuang/gear/GMainActivity.java
@@ -1,48 +1,69 @@
 package com.iskenhuang.gear;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
+import android.os.Handler;
+import android.os.Message;
+import android.os.StrictMode;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.iskenhuang.gear.ConnectMySQL.MyObj;
 
 public class GMainActivity extends Activity {
-
-    @Override
+	TextView textResult;
+	
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gmain_activity);
         init();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.gmain_activity, menu);
-        return true;
-    }
-
     private void init(){
-    	ListView listView = (ListView) findViewById(R.id.mylist);
-		String[] values = new String[] { "ConnectMySQL" };
-		
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, android.R.id.text1, values);
-		listView.setAdapter(adapter);
-		listView.setOnItemClickListener(new OnItemClickListener() {
-
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				Toast.makeText(getApplicationContext(), "Click ListItem Number " + position, Toast.LENGTH_LONG) .show();
-				if(position == 0){
-					Intent intent = new Intent();
-					intent.setClass(GMainActivity.this, DisplayResult.class);
-					startActivity(intent);
-				}
+    	textResult = (TextView) findViewById(R.id.textResult);
+    	textResult.setText("Waitting connected.");
+    	
+    	Button b = (Button) findViewById(R.id.buttonResult);
+    	b.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				handler.sendEmptyMessage(0);		
 			}
 		});
     }
+    
+    private void query(){
+    	if (android.os.Build.VERSION.SDK_INT > 9) {
+    	      StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+    	      StrictMode.setThreadPolicy(policy);
+    	}
+    	ConnectMySQL mysql = new ConnectMySQL();
+    	ArrayList results = new ArrayList();
+    	try {
+			results = mysql.getList();
+			String tempString = "";
+	    	
+	    	for(int i = 0; i < results.size(); i++){
+	    		MyObj obj = (MyObj) results.get(i);
+	    		tempString += obj.name;
+	    	}
+	    	
+	    	textResult.setText(tempString);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			textResult.setText("Connected error!!");
+		}
+    }
+    
+    private Handler handler = new Handler() {
+    	@Override
+    	public void handleMessage(Message msg) {
+    		query();
+    	}
+    };
 }
